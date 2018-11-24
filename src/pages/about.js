28788666514403303311d66f83ef74072429e867
mooping <mongkonchai4412@gmail.com>
2018-11-24 10:20:24 +0700

    initial gatsby project

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+
+class About extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>About</h1>
+      </Layout>
+    )
+  }
+}
+
+export default About
