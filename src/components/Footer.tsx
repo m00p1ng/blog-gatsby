1b8094fa99c1fb5013f6e64a863ca3ebd1d705dc
mooping <mongkonchai4412@gmail.com>
2018-12-07 00:19:30 +0700

    fixed footer problem

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Footer = () => (
-  <footer className="footer has-background-grey-dark">
-    <div className="has-text-centered has-text-white">
-      Made with ❤ using Bulma and Gatsby
+  <footer className="footer has-background-grey-dark" style={{ padding: '1.5rem' }}>
+    <div className="container">
+      <div className="is-pulled-left has-text-white">
+        © 2018 m00p1ng all right reserved.
+      </div>
+      <div className="is-pulled-right has-text-white">
+        Made with ❤ using Bulma and Gatsby
+      </div>
     </div>
   </footer>
 )
