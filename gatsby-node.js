e706da7f71420973b9691992715a41de7415e801
mooping <mongkonchai4412@gmail.com>
2018-12-12 22:05:07 +0700

    refactor gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,39 +4,111 @@ const _ = require('lodash')
 const {
   createPaginationPages,
   createLinkedPages,
-} = require("gatsby-pagination");
+} = require("gatsby-pagination")
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === 'MarkdownRemark') {
-    const slug = createFilePath({
-      node,
-      getNode,
-      basePath: 'src/content'
-    });
+const createIndexPaage = ({ createPage, posts, siteTitle, limit }) => {
+  const blogTemplate = path.resolve('./src/templates/blog.tsx')
 
-    const parseFolderName = (name) => {
-      const [date, postName] = name.split('___');
-      const dateFormat = date.replace(/-/g, "/")
-      return `${dateFormat}/${_.kebabCase(postName)}`
+  createPaginationPages({
+    createPage,
+    edges: posts,
+    limit,
+    pathFormatter: (pageNumber) => (
+      pageNumber === 1 ? '/' : `/page/${pageNumber}`
+    ),
+    component: blogTemplate,
+    context: {
+      siteTitle,
     }
+  })
+}
 
-    createNodeField({
-      node,
-      name: 'slug',
-      value: parseFolderName(slug)
-    });
-  }
+const createPublishedPage = ({ createPage, posts, siteTitle }) => {
+  const postTemplate = path.resolve('./src/templates/post.tsx')
+
+  createLinkedPages({
+    createPage,
+    edges: posts,
+    component: postTemplate,
+    edgeParser: edge => {
+      const {
+        fields: { slug },
+      } = edge.node;
+      return {
+        path: slug,
+        context: {
+          slug,
+          siteTitle,
+        },
+      }
+    },
+    circular: true,
+  })
+}
+
+const getUniqueTag = (posts) => {
+  const tags = [];
+  posts.forEach(({ node }) => {
+    node.frontmatter.tags.forEach((tag) => {
+      tags.push(tag);
+    })
+  })
+
+  return _.uniq(tags)
+}
+
+const createTagSimplePage = ({ createPage, posts, siteTitle }) => {
+  const tagTemplate = path.resolve('./src/templates/tag-simple.tsx')
+  const tags = getUniqueTag(posts)
+
+  tags.forEach(tag => {
+    const postsByTag = posts.filter(({ node }) =>
+      (node.frontmatter.tags.includes(tag))
+    )
+
+    createPage({
+      path: `/tags/${_.kebabCase(tag)}`,
+      component: tagTemplate,
+      context: {
+        tag,
+        postsByTag,
+        siteTitle,
+      }
+    })
+  })
+}
+
+const createTagPage = ({ createPage, posts, siteTitle, limit }) => {
+  const tagTemplate = path.resolve('./src/templates/tag.tsx')
+  const tags = getUniqueTag(posts)
+
+  tags.forEach(tag => {
+    const postsByTag = posts.filter(({ node }) =>
+      (node.frontmatter.tags.includes(tag))
+    )
+
+    createPaginationPages({
+      createPage,
+      edges: postsByTag,
+      limit,
+      pathFormatter: (pageNumber) => {
+        const tagPath = `/tags/${_.kebabCase(tag)}`
+        return (
+          pageNumber === 1 ? tagPath : `${tagPath}/page/${pageNumber}`
+        )
+      },
+      component: tagTemplate,
+      context: {
+        siteTitle,
+        tag,
+      }
+    })
+  })
 }
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-  const tags = []
-
-  const postTemplate = path.resolve('./src/templates/post.tsx')
-  const blogTemplate = path.resolve('./src/templates/blog.tsx')
-  const tagTemplate = path.resolve('./src/templates/tag.tsx')
-  const itemsPerPage = 10;
+  const ITEM_PER_PAGE = 10;
 
   return new Promise((resolve, reject) => {
     graphql(`
@@ -88,84 +160,50 @@ exports.createPages = ({ graphql, actions }) => {
         const siteTitle = result.data.site.siteMetadata.title
         const posts = result.data.allMarkdownRemark.edges
 
-        posts.forEach(({ node }) => {
-          node.frontmatter.tags.forEach((tag) => {
-            tags.push(tag);
-          })
-
-          createLinkedPages({
-            createPage,
-            edges: posts,
-            component: postTemplate,
-            edgeParser: edge => {
-              const {
-                fields: { slug },
-              } = edge.node;
-              return {
-                path: slug,
-                context: {
-                  slug,
-                  siteTitle,
-                },
-              };
-            },
-            circular: true,
-          })
-        })
-
-        createPaginationPages({
+        createIndexPaage({
           createPage,
-          edges: posts,
-          limit: itemsPerPage,
-          pathFormatter: (pageNumber) => (
-            pageNumber === 1 ? '/' : `/page/${pageNumber}`
-          ),
-          component: blogTemplate,
-          context: {
-            siteTitle,
-          }
+          posts,
+          siteTitle,
+          limit: ITEM_PER_PAGE,
         })
 
-        // Create each tag page
-        _.uniq(tags)
+        createPublishedPage({
+          createPage,
+          posts,
+          siteTitle,
+        })
 
-
-        // For Tag simple generate page
-
-        /* tags.forEach(tag => {
-          createPage({
-            path: `/tags/${_.kebabCase(tag)}`,
-            component: tagTemplate,
-            context: {
-              tag
-            }
-          })
-        }) */
-
-        tags.forEach(tag => {
-          const postsByTag = posts.filter(({ node }) =>
-            (node.frontmatter.tags.includes(tag))
-          )
-
-          createPaginationPages({
-            createPage,
-            edges: postsByTag,
-            limit: itemsPerPage,
-            pathFormatter: (pageNumber) => {
-              const tagPath = `/tags/${_.kebabCase(tag)}`
-              return (
-                pageNumber === 1 ? tagPath : `${tagPath}/page/${pageNumber}`
-              )
-            },
-            component: tagTemplate,
-            context: {
-              siteTitle,
-              tag,
-            }
-          })
+        createTagPage({
+          createPage,
+          posts,
+          siteTitle,
+          limit: ITEM_PER_PAGE,
         })
 
         resolve()
       })
   })
+}
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: 'src/content'
+    });
+
+    const parseFolderName = (name) => {
+      const [date, postName] = name.split('___');
+      const dateFormat = date.replace(/-/g, "/")
+      return `${dateFormat}/${_.kebabCase(postName)}`
+    }
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: parseFolderName(slug)
+    });
+  }
 }
