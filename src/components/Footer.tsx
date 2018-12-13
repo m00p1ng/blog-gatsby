fed619949128c7fe60c036e561d2f5522ad94b2a
mooping <mongkonchai4412@gmail.com>
2018-12-13 10:57:17 +0700

    update footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const FooterLink = styled.a`
+  color: orange;
+  &:hover {
+    color: orange;
+    text-decoration: underline;
+  }
+`
 
 const Footer = () => (
   <footer className="footer">
     <div className="has-text-centered has-text-white">
       <p>m00p1ng © 2018 all right reserved.</p>
-      <small>Made with ♥️ using Bulma and Gatsby.</small>
+      <small>Made with ♥️ using{' '}
+        <FooterLink href="https://gatsbyjs.org">Gatsby</FooterLink>
+        {' '}+{' '}
+        <FooterLink href="https://bulma.io">Bulma</FooterLink>.
+      </small>
     </div>
   </footer>
 )
