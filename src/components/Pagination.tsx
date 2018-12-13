000828bfbb9cecdd070d29f93673c2db12798654
mooping <mongkonchai4412@gmail.com>
2018-12-14 06:56:39 +0700

    rearrange code and remove duplicate style

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -13,43 +13,53 @@ const PaginationWrapper = styled.div`
   margin: 1rem 0;
 `
 
-const PageOfWrapper = styled.div`
-  display:inline-flex;
-  align-items: center;
-  justify-content: center;
-`
-
-const PaginationTemplate = ({ next, prev, page, pages }: Props) => {
-
-  return (
-    <PaginationWrapper>
-      <nav className="columns is-mobile">
-        <div className="column">
-          {prev && (
-            <Link to={prev} rel="prev">
-              <h1 className="title pagination-font pagination-link pagination-link__left">
-                ← Newer
-              </h1>
-            </Link>
-          )}
-        </div>
-        <PageOfWrapper className="column pagination-page">
-          <h1 className="subtitle pagination-font">
-            Page {page} of {pages}
-          </h1>
-        </PageOfWrapper>
-        <div className="column has-text-right">
-          {next && (
-            <Link to={next} rel="next">
-              <h1 className="title pagination-font pagination-link pagination-link__right">
-                Older →
-              </h1>
-            </Link>
-          )}
-        </div>
-      </nav>
-    </PaginationWrapper>
-  )
+interface NavLinkProps {
+  link: string
+  rel: string
+  className: string
+  text: string
 }
+
+const NavLink = ({ link, rel, className, text }: NavLinkProps) => (
+  <Link to={link} rel={rel}>
+    <h1 className={
+      `title pagination-font pagination-link ${className}`
+    }>
+      {text}
+    </h1>
+  </Link>
+)
+
+const PaginationTemplate = ({ next, prev, page, pages }: Props) => (
+  <PaginationWrapper>
+    <nav className="columns is-mobile">
+      <div className="column">
+        {prev && (
+          <NavLink
+            link={prev}
+            rel={'prev'}
+            className="pagination-link__left"
+            text="← Newer"
+          />
+        )}
+      </div>
+      <div className="column pagination-page">
+        <h1 className="subtitle pagination-font">
+          Page {page} of {pages}
+        </h1>
+      </div>
+      <div className="column has-text-right">
+        {next && (
+          <NavLink
+            link={next}
+            rel={'next'}
+            className="pagination-link__right"
+            text="Older →"
+          />
+        )}
+      </div>
+    </nav>
+  </PaginationWrapper>
+)
 
 export default PaginationTemplate
