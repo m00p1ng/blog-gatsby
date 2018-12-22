0e16644aece370f514c2720f3a58fe7871e94300
mooping <mongkonchai4412@gmail.com>
2018-12-22 09:11:14 +0700

    add icon for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import styled from 'styled-components'
 
 const FooterLink = styled.a`
-  color: orange;
+  color: white;
   &:hover {
     color: orange;
     text-decoration: underline;
@@ -13,15 +14,42 @@ const FooterWrapper = styled.footer`
   padding: 3rem;
 `
 
+const SubtitleWrapper = styled.small`
+  display: flex;
+  justify-content: center;
+`
+
+const HeartIcon = styled(FontAwesomeIcon).attrs({
+  icon: 'heart'
+})`
+  color: red;
+`
+
+const CodeIcon = styled(FontAwesomeIcon).attrs({
+  icon: 'code'
+})`
+  color: orange;
+  margin-right: 5px;
+  margin-bottom: 2px;
+`
+
 const Footer = () => (
   <FooterWrapper>
     <div className="has-text-centered has-text-white">
       <p>m00p1ng © 2018 all right reserved.</p>
-      <small>Made with ♥️ using&nbsp;
+      <SubtitleWrapper>
+        <span className="icon">
+          <CodeIcon />
+        </span>
+        with
+        <span className="icon">
+          <HeartIcon />
+        </span>
+        using&nbsp;
         <FooterLink href="https://gatsbyjs.org">Gatsby</FooterLink>
         &nbsp;+&nbsp;
         <FooterLink href="https://bulma.io">Bulma</FooterLink>.
-      </small>
+      </SubtitleWrapper>
     </div>
   </FooterWrapper>
 )
--- src/utils/fontawesome.ts
+++ src/utils/fontawesome.ts
@@ -6,6 +6,8 @@ import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons/faAngleDoub
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons/faAngleDoubleRight'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons/faArrowUp'
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons/faCalendarAlt'
+import { faCode } from '@fortawesome/free-solid-svg-icons/faCode'
+import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart'
 
 library.add(
   faCalendarAlt,
@@ -14,4 +16,6 @@ library.add(
   faArrowUp,
   faFacebookF,
   faTwitter,
+  faHeart,
+  faCode,
 )
